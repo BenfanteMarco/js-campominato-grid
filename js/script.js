77593997e1e6d602541a58be581e475a1a19dc71
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-11-30 14:46:53 +0100

    created function to generate grid to correct

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ function createCell(num){
 
     element.innerText = num;
 
-    element.addEventListener('click', function(){
-        this.classList.toggle('clicked');
-    })
     return element;
 }
+
+// Creo funzione che mi genera la grid
+function createNewGrid(){
+    const newGrid = document.getElementById('grid');
+    newGrid.innerHTML = '';
+}
+
+const generaGriglia = document.getElementById('play');
+generaGriglia.addEventListener('click', createNewGrid);
 
 // Recupero l'elemento che contienela griglia
 const grid = document.getElementById('grid');
@@ -17,5 +23,11 @@ const grid = document.getElementById('grid');
 // Eseguo un for
 for(let i=0; i<100; i++){
     let square = createCell(i+1);
+
+    square.addEventListener('click', function(){
+        this.classList.toggle('clicked');
+    })
+
     grid.appendChild(square);
 }
+
